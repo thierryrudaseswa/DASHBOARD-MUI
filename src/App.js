d5a400ed77e148c0bf9cca69fc1e0f5ddc1dec17
BillBenon <DushimeBillBenon@gmail.com>
2023-10-02 12:17:40 +0200

    The best dashboard for the Admin created with advanced MUI and react js

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { ColorModeContext,useMode } from './theme';
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from './scenes/global/Sidebar';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/Team";
+import Contacts from './scenes/contacts';
+import Invoices from './scenes/invoices';
+import Form from './scenes/form';
+import Calendar from './scenes/calendar';
+import FAQ from './scenes/faq';
+import Pie from './scenes/pie';
+import Bar from './scenes/Bar';
+import Line from './scenes/line';
+import Geography from "./scenes/geography";
+
+function App() {
+  const [theme,colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <div className="app">
+        <Sidebar />
+        <main className="content">
+          <Topbar />
+          <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/invoices" element={<Invoices />} />
+          <Route path='/form' element={<Form />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path='/Pie' element={<Pie />} />
+          <Route path='/Bar' element={<Bar />} />
+          <Route path='/Line' element={<Line />} />
+          <Route path="/geography" element={<Geography />} />
+          </Routes>
+        </main>
+      </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+
+export default App;
